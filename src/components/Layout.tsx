1f038a41e5a05c2ec883de891754df8dab48af4b
abhishekmishra2903 <68509901+abhishekmishra2903@users.noreply.github.com>
2025-06-03 15:01:48 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ import Header from './Header';
 const Layout = () => {
   return <div className="flex h-screen bg-gray-50">
       <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="w-64 h-full bg-white">
         <Header />
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 p-6 px-0 mx-[50px]">
           <Outlet />
