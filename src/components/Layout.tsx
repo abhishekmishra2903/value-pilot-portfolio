7d80a3f312f2d3a8cd39b9bb111d98631695683c
abhishekmishra2903 <68509901+abhishekmishra2903@users.noreply.github.com>
2025-06-03 14:56:59 +0530

    Update Layout.tsx
    Updated the sidebar className.
    Trying to solve the issue of main content overlapping with the side-bar.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ import Header from './Header';
 const Layout = () => {
   return <div className="flex h-screen bg-gray-50">
       <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden ml-64 md:ml-0">
+      <div className="flex-1 flex flex-col overflow-hidden">
         <Header />
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 p-6 px-0 mx-[50px]">
           <Outlet />
